3d07f708ef4156fd977660c16611b82c15b00fb9
root <root@qq.com>
2022-09-20 08:40:44 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author lypengineer
+ * @create 2022-09-20 8:13
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
